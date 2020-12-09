53183da44c71d2291970ab222681452c03b7577e
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-12-08 20:39:24 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { LogService } from '../log.service';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+  //providers: [DataService, LogService]
 })
 export class LoginComponent implements OnInit {
+  usuarios : string[] = [];
 
-  constructor() { }
+  validacion = false;
 
-  ngOnInit() {
+  constructor(private httpService: HttpService, private logService: LogService, private dataService: DataService) { }
+
+  iniciarSesion(usr:string, psw:string){
+    if(usr != '' && psw != '')
+    {
+      this.httpService.getDatos().subscribe((data:Response) =>{
+      this.validacion = true;
+      
+      for(let key in data){
+        
+        if(data[key].email == usr && data[key].pass == psw){
+          alert('datos correctos')
+          this.validacion = false;
+          
+        } 
+      }
+      if (this.validacion==true) {
+        alert('Usuario y/o constraseña incorrectos')
+      }
+    });
+    }
   }
+
+  ngOnInit(){
+    this.usuarios = this.dataService.getUsers();
+  }
+
+  onlog(valor){
+    this.logService.escribirLog(valor);
+  }
+
+  //enviarForm(form){
+  //  console.log(form)
+  //}
 
 }
