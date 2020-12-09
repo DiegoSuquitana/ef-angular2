6b10e09c8b2dc7324e93654a2fcc66e0f98a04c5
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-12-08 21:57:15 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
-import { LoginComponent } from './login/login.component';
 import { HttpService } from './http.service';
 import { LogService } from './log.service';
 import { DataService } from './data.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { VistaPrincipalComponent } from './vista-principal/vista-principal.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraSuperiorComponent,
-    LoginComponent
+    LoginComponent,
+    VistaPrincipalComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [LogService,HttpService,DataService],
   bootstrap: [AppComponent]
