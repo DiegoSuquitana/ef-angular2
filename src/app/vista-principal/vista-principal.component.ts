802f5a518a9b1e4981f706484aaf4404f116b6fb
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-02 01:02:11 -0500

    Update vista-principal.component.ts

--- src/app/vista-principal/vista-principal.component.ts
+++ src/app/vista-principal/vista-principal.component.ts
@@ -1,3 +1,4 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from '../http.service';
@@ -9,9 +10,49 @@ import { HttpService } from '../http.service';
 })
 export class VistaPrincipalComponent implements OnInit {
 
+  productos: any[]=[];
+  subproducto: any[] = this.httpService.subproducto;
+
   constructor(private router:Router, private httpService: HttpService) { }
 
+
+  buscador(texto){
+    this.subproducto = [];
+    for(var i=0; i<this.httpService.subproducto.length; i++){
+      var cadena;
+      cadena = this.httpService.subproducto[i].nombre;
+      
+      if(cadena.indexOf(texto)!=-1){
+        this.httpService.subproducto[i].vista = true
+        this.subproducto.push(this.httpService.subproducto[i])
+      }else{
+        this.httpService.subproducto[i].vista = false
+      }
+    }
+  }
+
+  
   ngOnInit() {
+
+    this.httpService.getDatos().subscribe((data: Response)=>{
+     if(this.httpService.subproducto.length==0){
+      for(let key in data){
+        if(data[key].tipo == 'producto' && data[key]!=null){
+          console.log(data[key])
+          this.productos.push(data[key]);
+          
+        }
+        
+      }
+        
+        this.subproducto = this.productos;
+        this.httpService.subproducto = this.subproducto;
+        
+      
+      }
+    }
+    )
+ 
   }
 
 }
