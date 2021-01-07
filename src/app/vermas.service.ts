ae3f5d17001b771ce8170be9cfd60c26ba188566
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-06 22:59:04 -0500

    Create vermas.service.ts

---
+++ src/app/vermas.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class VermasService {
+
+  imagen: string;
+  nombre: string;
+  precio: string;
+  stock: string; 
+
+  constructor() { }
+
+}
