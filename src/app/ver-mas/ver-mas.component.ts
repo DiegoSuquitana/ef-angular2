2b52fe79289dfd436f09dad82975881c2306d1bf
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-06 22:58:58 -0500

    Create ver-mas.component.ts

---
+++ src/app/ver-mas/ver-mas.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {VermasService} from '../vermas.service';
+
+@Component({
+  selector: 'app-ver-mas',
+  templateUrl: './ver-mas.component.html',
+  styleUrls: ['./ver-mas.component.css']
+})
+export class VerMasComponent implements OnInit {
+  imagen = this.verMas.imagen;
+  nombre = this.verMas.nombre;
+  precio = this.verMas.precio;
+  stock = this.verMas.stock;
+
+  constructor(private router:Router, private verMas: VermasService) { }
+
+  atras(){
+    this.router.navigate(['/vista-principal']);
+  }
+
+  ngOnInit() {
+  }
+
+}
