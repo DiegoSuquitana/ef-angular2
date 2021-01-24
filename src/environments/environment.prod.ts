de9e65b6773e584fb3ac771c494720d363d6c411
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-23 22:33:03 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+
+   firebase : {
+    apiKey: "AIzaSyD5aHVx4sumLxOFETfy5lw4rGHF_M72Kec",
+    authDomain: "tienda-online-efnextu.firebaseapp.com",
+    databaseURL: "https://tienda-online-efnextu.firebaseio.com",
+    projectId: "tienda-online-efnextu",
+    storageBucket: "tienda-online-efnextu.appspot.com",
+    messagingSenderId: "1095468038148",
+    appId: "1:1095468038148:web:c95120df9bfce0122a5d51",
+    measurementId: "G-SPG9BMHG07"
+  }
+
 };
