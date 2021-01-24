ed5ff9ac3c38a890d5c574c428ceade64757d2ee
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-23 22:33:06 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,17 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase : {
+    apiKey: "AIzaSyD5aHVx4sumLxOFETfy5lw4rGHF_M72Kec",
+    authDomain: "tienda-online-efnextu.firebaseapp.com",
+    databaseURL: "https://tienda-online-efnextu.firebaseio.com",
+    projectId: "tienda-online-efnextu",
+    storageBucket: "tienda-online-efnextu.appspot.com",
+    messagingSenderId: "1095468038148",
+    appId: "1:1095468038148:web:c95120df9bfce0122a5d51",
+    measurementId: "G-SPG9BMHG07"
+  }
+
 };
