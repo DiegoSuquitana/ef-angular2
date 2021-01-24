ce0f8eb46ed7a91905817893aa8db34bc83a092f
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2021-01-23 22:32:50 -0500

    Update carrito.component.ts

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {CarritoService} from '../carrito.service';
 import {Router} from '@angular/router'
+import { HttpService } from '../http.service';
+import {VermasService} from '../vermas.service';
+//import {FirebaseServiceService} from '../services/firebase-service.service';
+
 
 @Component({
   selector: 'app-carrito',
@@ -9,23 +13,35 @@ import {Router} from '@angular/router'
 })
 export class CarritoComponent implements OnInit {
 
-  car: any[]=[];
+  config : any;
+  collection =  {count: 60, data:[]};
+
+  public car: any[]=[];
   public total;
 
-
-  constructor(private carro: CarritoService, private router: Router) { }
+  constructor(private carro: CarritoService, 
+    private router: Router, 
+    private httpService: HttpService,  
+    private verMas: VermasService
+  //  private FirebaseService: FirebaseServiceService
+    ) { }
 
   cancelar(){
     this.router.navigate(['/vista-principal']);
     this.carro.estado = 0;
   }
 
-  pagar(){
+  pagar(nombre){
+   
     this.car = [];
     this.carro.carrito = [];
     this.router.navigate(['/vista-principal']);
     this.carro.estado = 0;
     this.carro.total=0;
+    console.log(nombre[0].stock);
+    console.log(nombre);
+    
+    this.httpService.actualizarProductos(nombre);
   }
 
   ngOnInit() {
